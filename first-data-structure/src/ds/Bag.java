5c19ce862c009ec765f58aa0f8732bf0e99cb2d3
Josh Archer <jarcher@greenriver.edu>
2022-01-06 09:11:08 -0800

    Added an implementation of Bag.contains(). Added full Javadocs to the class.

--- first-data-structure/src/ds/Bag.java
+++ first-data-structure/src/ds/Bag.java
@@ -2,12 +2,26 @@ package ds;
 
 import java.util.Arrays;
 
+/**
+ * A simple data structure with the properties:
+ * - fixed size
+ * - no access by index
+ * - basic methods to add, remove or query for elements
+ *
+ * @author Josh Archer
+ * @version 1.0
+ */
 public class Bag implements IBag
 {
     //fields to store the data and track the status of the bag?
     private Object[] dataArray;
     private int nextAvailableIndex;
 
+    /**
+     * Creates a new bag with the specified capacity
+     * @param capacity the maximum elements that can be
+     *                 placed in the bag
+     */
     public Bag(int capacity)
     {
         //preconditions
@@ -19,6 +33,13 @@ public class Bag implements IBag
         dataArray = new Object[capacity];
     }
 
+    /**
+     * Adds a new element to the bag. If the bag
+     * is at capacity then no element is added.
+     *
+     * @param element the element to put in the bag
+     * @return true if the element was added, otherwise false
+     */
     @Override
     public boolean add(Object element)
     {
@@ -33,36 +54,79 @@ public class Bag implements IBag
         return true;
     }
 
+    /**
+     * Returns the number of elements in the bag.
+     * @return the element count
+     */
     @Override
     public int size()
     {
         return nextAvailableIndex;
     }
 
+    /**
+     * Returns the total elements that can fit in
+     * the bag before it is full.
+     * @return the max capacity of the bag
+     */
     @Override
     public int capacity()
     {
         return dataArray.length;
     }
 
+    /**
+     * Reports whether an element is in the bag or not.
+     *
+     * @param element the element to search for
+     * @return true if found, or false otherwise
+     */
     @Override
     public boolean contains(Object element)
     {
-        return false;
+        //NOTE: we only need to loop through the occupied indices
+        //and not to the end of the array necessarily
+        for (int i = 0; i < nextAvailableIndex; i++)
+        {
+            //NOTE: it is best to use .equals() and not ==
+            if (dataArray[i].equals(element))
+            {
+                return true; //found it!
+            }
+        }
+        return false; //did not find it!
     }
 
+    /**
+     * Removes the first occurrence of the element
+     * in the bag, if found. Comparisons are made
+     * using the .equals() method of the input
+     * element.
+     *
+     * @param element the element to search for
+     * @return true if the element is found and removed,
+     * or false otherwise
+     */
     @Override
     public boolean remove(Object element)
     {
         return false;
     }
 
+    /**
+     * Removes all elements from the bag. After calling clear,
+     * new elements are then placed in an empty bag.
+     */
     @Override
     public void clear()
     {
 
     }
 
+    /**
+     * Prints out a visualization of the bag as a string.
+     * @return the bag contents
+     */
     @Override
     public String toString()
     {
