50457f8e2401d377db5edf14e449e3cad0e20139
Josh Archer <jarcher@greenriver.edu>
2022-03-07 13:49:31 -0800

    Added an example of overriding equals().

--- hashtables/src/equals/TestSpecs.java
+++ hashtables/src/equals/TestSpecs.java
@@ -5,7 +5,9 @@ public class TestSpecs
     public static void main(String[] args)
     {
         ComputerSpec defaultSpec = new ComputerSpec();
-        ComputerSpec mySpec = new ComputerSpec("Intel i7 7700K", 64, true, 5.0);
+        ComputerSpec mySpec = new ComputerSpec("Intel i9 9900K", 64, true, 5.0);
+        ComputerSpec multitaskingSpec = new ComputerSpec("AMD Ryzen", 64, true, 5.0);
+        ComputerSpec serverSpec = new ComputerSpec("Intel i9 9900K", 128, false, 5.0);
 
         System.out.println(defaultSpec);
         System.out.println(mySpec);
@@ -18,5 +20,23 @@ public class TestSpecs
 
         //let's use the default Object.equals()
         System.out.println(defaultSpec.equals(mySpec));
+        System.out.println();
+
+        //equivalence relation?
+        //reflexive
+        System.out.println("Reflexive: " + mySpec.equals(mySpec)); //a.equals(a)
+        System.out.println();
+
+        //symmetric
+        System.out.println("Symmetric: " + mySpec.equals(defaultSpec)); //a.equals(b)
+        System.out.println("Symmetric: " + defaultSpec.equals(mySpec)); //b.equals(a)
+        System.out.println("Symmetric: " + multitaskingSpec.equals(defaultSpec)); //a.equals(b)
+        System.out.println("Symmetric: " + defaultSpec.equals(multitaskingSpec)); //b.equals(a)
+        System.out.println();
+
+        //transitive
+        System.out.println("Transitive: " + mySpec.equals(defaultSpec)); //a.equals(b)
+        System.out.println("Transitive: " + defaultSpec.equals(serverSpec)); //b.equals(c)
+        System.out.println("Transitive: " + mySpec.equals(serverSpec)); //a.equals(c)
     }
 }
